17bdc1e1a32a074d0e07b05e98c67d6514659d12
Charles <root@pips>
2023-11-28 21:21:28 -0700

    Added Langchain

--- backend/sqlchain.py
+++ backend/sqlchain.py
@@ -1,5 +1,7 @@
 import psycopg2
-
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import StrOutputParser
 
 class SQLChain:
     def __init__(self):
@@ -19,10 +21,18 @@ class SQLChain:
         except psycopg2.Error as e:
             print(f"Unable to connect to the database: {e}")
 
-    def create_query(self, query):
+    def create_table(self, csv):
         # Create a query with openai
+        template = """Based on the first two lines of csv below, write a query to create a table:
+        {csv}
+        SQL Query:"""
+        prompt = ChatPromptTemplate(template)
+        model = ChatOpenAI(prompt)
+        chain = prompt | model | StrOutputParser()
+        chain.invoke{{'csv": {csv}'}}
+        
 
-        pass
+
     
     def execute_query(self, query):
         # Execute a query
