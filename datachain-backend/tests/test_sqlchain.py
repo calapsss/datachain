4c896ae25035fedd8626876b29a7276caa9ddc3b
Charles Calapini <charles@calapini.com>
2023-11-29 06:34:45 -0700

    Added some unittests

--- datachain-backend/tests/test_sqlchain.py
+++ datachain-backend/tests/test_sqlchain.py
@@ -1,19 +1,35 @@
 import pytest
-from unittest.mock import patch, MagicMock
-from sqlchain import SQLChain
+from unittest.mock import MagicMock
+from datachain_backend.sqlchain import SQLChain
 
-class TestSQLChain:
-    @pytest.fixture
-    def setup(self):
-        with patch('psycopg2.connect') as mock_connect:
-            mock_connect.return_value = MagicMock()
-            sqlchain = SQLChain()
-            sqlchain.connect()
-        return sqlchain
+def test_connect(mocker):
+    # Arrange
+    mock_connect = mocker.patch('psycopg2.connect', return_value=MagicMock())
+    sqlchain = SQLChain()
 
-    def test_connect(self, setup):
-        assert setup.connection is not None
+    # Act
+    sqlchain.connect()
 
-    def test_create_table(self, setup):
-        # Add your test logic here
-        pass
+    # Assert
+    mock_connect.assert_called_once_with(
+        database=sqlchain.database, 
+        user=sqlchain.user, 
+        password=sqlchain.password, 
+        host=sqlchain.host,
+        port=sqlchain.port
+    )
+
+def test_disconnect(mocker):
+    # Arrange
+    mock_connection = MagicMock()
+    mocker.patch('psycopg2.connect', return_value=mock_connection)
+    sqlchain = SQLChain()
+    sqlchain.connect()
+
+    # Act
+    sqlchain.disconnect()
+
+    # Assert
+    mock_connection.close.assert_called_once()
+
+# Add more tests as needed
