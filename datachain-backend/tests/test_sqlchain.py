9a93ceca12b9dbeb256b23d78db32b934679927c
Charles Calapini <charles@calapini.com>
2023-11-29 06:36:15 -0700

    fixed unittest

--- datachain-backend/tests/test_sqlchain.py
+++ datachain-backend/tests/test_sqlchain.py
@@ -1,35 +1,41 @@
-import pytest
-from unittest.mock import MagicMock
+import unittest
+from unittest.mock import patch, MagicMock
 from datachain_backend.sqlchain import SQLChain
 
-def test_connect(mocker):
-    # Arrange
-    mock_connect = mocker.patch('psycopg2.connect', return_value=MagicMock())
-    sqlchain = SQLChain()
+class TestSQLChain(unittest.TestCase):
+    @patch('psycopg2.connect')
+    def test_connect(self, mock_connect):
+        # Arrange
+        mock_connect.return_value = MagicMock()
+        sqlchain = SQLChain()
 
-    # Act
-    sqlchain.connect()
+        # Act
+        sqlchain.connect()
 
-    # Assert
-    mock_connect.assert_called_once_with(
-        database=sqlchain.database, 
-        user=sqlchain.user, 
-        password=sqlchain.password, 
-        host=sqlchain.host,
-        port=sqlchain.port
-    )
+        # Assert
+        mock_connect.assert_called_once_with(
+            database=sqlchain.database, 
+            user=sqlchain.user, 
+            password=sqlchain.password, 
+            host=sqlchain.host,
+            port=sqlchain.port
+        )
 
-def test_disconnect(mocker):
-    # Arrange
-    mock_connection = MagicMock()
-    mocker.patch('psycopg2.connect', return_value=mock_connection)
-    sqlchain = SQLChain()
-    sqlchain.connect()
+    @patch('psycopg2.connect')
+    def test_disconnect(self, mock_connect):
+        # Arrange
+        mock_connection = MagicMock()
+        mock_connect.return_value = mock_connection
+        sqlchain = SQLChain()
+        sqlchain.connect()
 
-    # Act
-    sqlchain.disconnect()
+        # Act
+        sqlchain.disconnect()
 
-    # Assert
-    mock_connection.close.assert_called_once()
+        # Assert
+        mock_connection.close.assert_called_once()
 
-# Add more tests as needed
+    # Add more tests as needed
+
+if __name__ == '__main__':
+    unittest.main()
